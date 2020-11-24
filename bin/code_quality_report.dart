4b8f27361942bfe571bbc84a3e02f9c3428545b4
Adrian Jagielak <adrianjagielak@gmail.com>
2020-11-24 18:38:17 +0100

    Initial commit

---
+++ bin/code_quality_report.dart
@@ -0,0 +1,5 @@
+import 'package:code_quality_report/code_quality_report.dart' as code_quality_report;
+
+void main(List<String> arguments) {
+  print('Hello world: ${code_quality_report.calculate()}!');
+}
---
+++ test/code_quality_report_test.dart
@@ -0,0 +1,8 @@
+import 'package:code_quality_report/code_quality_report.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
